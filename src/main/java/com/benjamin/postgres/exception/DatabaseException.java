6a7591a471aef02925241f214717cee3ac145f54
benjamin <bnjmnsepulveda@gmail.com>
2018-11-28 14:02:02 -0300

    se agregan excepciones

---
+++ src/main/java/com/benjamin/postgres/exception/DatabaseException.java
@@ -0,0 +1,22 @@
+
+package com.benjamin.postgres.exception;
+
+/**
+ *
+ * @author benjamin
+ */
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DatabaseException(Throwable cause) {
+        super(cause);
+    }
+
+}
